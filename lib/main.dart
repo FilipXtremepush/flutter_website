fe61f4cf35316e50325bbce13e4f8909cb90e821
Filip Napiorkowski <filip.napiorkowski@xtremepush.com>
2024-05-16 12:27:27 +0100

    changed tests again

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:js/js.dart';
 
-// @JS('console.log') // annotates `log` function to call `console.log`
-// external void log(dynamic str);
+@JS('console.log') // annotates `log` function to call `console.log`
+external void log(dynamic str);
 // @JS('xtremepush') // annotates xtremepush function to call xtremepush
 // external void xtremepush(String event, String nameOfEvent);
 
 void main() {
   runApp(const MyApp());
-  // log("Hello World from JavaScript");
+  log("Hello World from JavaScript");
   // xtremepush('event', 'test');
 }
 
